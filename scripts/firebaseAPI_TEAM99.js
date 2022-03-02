ff87575fa0d80ac4b473add7d7d17607a4110304
Jackie Ma <majackie@live.com>
2022-03-01 22:02:21 -0800

    uncomment

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyD4AcHWv5y9xIuO7g3NyEGNEZiK-rra_uI",
+    authDomain: "fir-comp1800-82b0f.firebaseapp.com",
+    projectId: "fir-comp1800-82b0f",
+    storageBucket: "fir-comp1800-82b0f.appspot.com",
+    messagingSenderId: "145998497909",
+    appId: "1:145998497909:web:1d6412d5d06b67840b503c"
 };
 
 //--------------------------------------------
